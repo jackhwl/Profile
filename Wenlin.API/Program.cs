f2ead11e34cbfaf9125de381d32c3a09d6de8295
Wenlin Huang <jackhwl@hotmail.com>
2023-03-09 18:28:46 -0500

    Add Infrastructure Layer

--- Wenlin.API/Program.cs
+++ Wenlin.API/Program.cs
@@ -1,6 +1,10 @@
+using Wenlin.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+string connectionString = builder.Configuration.GetConnectionString("WenlinConnection");
+builder.Services.AddInfrastructure(connectionString);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
---
+++ Wenlin.Infrastructure/ServiceRegistration.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Wenlin.Domain;
+
+namespace Wenlin.Infrastructure;
+public static class ServiceRegistration
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, string connectionString)
+    {
+        services.AddDbContext<WenlinContext>(options =>
+        {
+            options.UseSqlServer(connectionString, sqlOptions =>
+            {
+                sqlOptions.MigrationsAssembly(typeof(ServiceRegistration).Assembly.FullName);
+            });
+        });
+
+        return services;
+    }
+}

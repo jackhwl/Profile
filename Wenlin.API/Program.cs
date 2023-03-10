35b0f6c2ecf15f1c236514c817dc8a365b9b3ad4
Wenlin Huang <jackhwl@hotmail.com>
2023-03-10 18:45:29 -0500

    move config service & pipeline to helper file

--- Wenlin.API/Program.cs
+++ Wenlin.API/Program.cs
@@ -1,36 +1,9 @@
-using Wenlin.Infrastructure;
+using Wenlin.API;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add configuration sources
-if (builder.Environment.IsDevelopment())
-{
-    builder.Configuration.AddUserSecrets<Program>();
-}
-
-// Add services to the container.
-string connectionString = builder.Configuration.GetConnectionString("WenlinConnection");
-builder.Services.AddInfrastructure(connectionString);
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
+var app = builder
+       .ConfigureServices()
+       .ConfigurePipeline();
 
 app.Run();
---
+++ Wenlin.API/StartupHelperExtensions.cs
@@ -0,0 +1,46 @@
+﻿using Wenlin.Infrastructure;
+
+namespace Wenlin.API;
+
+internal static class StartupHelperExtensions
+{
+    // Add services to the container
+    public static WebApplication ConfigureServices(this WebApplicationBuilder builder)
+    {
+        // Add configuration sources
+        if (builder.Environment.IsDevelopment())
+        {
+            builder.Configuration.AddUserSecrets<Program>();
+        }
+
+        // Add services to the container.
+        string connectionString = builder.Configuration.GetConnectionString("WenlinConnection");
+        builder.Services.AddInfrastructure(connectionString);
+
+        builder.Services.AddControllers();
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+        builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+        return builder.Build();
+    }
+
+    // Configure the request/response pipelien
+    public static WebApplication ConfigurePipeline(this WebApplication app)
+    {
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+
+        app.MapControllers();
+
+        return app;
+    }
+}

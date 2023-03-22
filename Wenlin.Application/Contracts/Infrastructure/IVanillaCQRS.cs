2cd23aab50c05ee401ab335e43eb4c11318bd948
Wenlin Huang <jackhwl@hotmail.com>
2023-03-21 20:35:26 -0400

    Vanilla CQRS Interface

---
+++ Wenlin.Application/Contracts/Infrastructure/IVanillaCQRS.cs
@@ -0,0 +1,21 @@
+﻿namespace Wenlin.Application.Contracts.Infrastructure;
+
+public interface IQueryHandler<in TQuery, TQueryResult>
+{
+    Task<TQueryResult> Handle(TQuery query, CancellationToken cancellation);
+}
+
+public interface IQueryDispatcher
+{
+    Task<TQueryResult> Dispatch<TQuery, TQueryResult>(TQuery query, CancellationToken cancellation);
+}
+
+public interface ICommandHandler<in TCommand, TCommandResult>
+{
+    Task<TCommandResult> Handle(TCommand command, CancellationToken cancellation);
+}
+
+public interface ICommandDispatcher
+{
+    Task<TCommandResult> Dispatch<TCommand, TCommandResult>(TCommand command, CancellationToken cancellation);
+}

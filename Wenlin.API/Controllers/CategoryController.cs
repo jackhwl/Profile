21d57b1bbcd1289ae254665d4bc32da46511babd
Wenlin Huang <jackhwl@hotmail.com>
2023-04-05 10:19:10 -0400

    Add Utils with GetValidationProblemDetails method

--- Wenlin.API/Controllers/CategoryController.cs
+++ Wenlin.API/Controllers/CategoryController.cs
@@ -1,6 +1,7 @@
 ﻿using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Infrastructure;
+using Wenlin.API.Helpers;
 using Wenlin.Application.Features.Categories.Commands.CreateCategory;
 using Wenlin.Application.Features.Categories.Commands.DeleteCategory;
 using Wenlin.Application.Features.Categories.Commands.UpdateCategory;
@@ -50,33 +51,19 @@ public class CategoryController : ControllerBase
             if (response.NotFound) return NotFound();
 
             if (response.ValidationErrors != null)
-            //{                // create a validation problem details object
-            //    //var validationProblemDetails = new ValidationProblemDetails();
-            //    // https://www.strathweb.com/2018/07/centralized-exception-handling-and-request-validation-in-asp-net-core/
-            //    var problemDetailsFactory = HttpContext.RequestServices.GetRequiredService<ProblemDetailsFactory>();
-
-            //    var validationProblemDetails = problemDetailsFactory.CreateValidationProblemDetails(HttpContext, ModelState);
-
-            //    // add additional info not added by default
-            //    validationProblemDetails.Detail = "See the errors field for details.";
-            //    validationProblemDetails.Instance = HttpContext.Request.Path;
-
-            //    // report invalid model state responses as validation issues
-            //    validationProblemDetails.Type = "https://courselibrary.com/modelvalidationproblem";
-            //    validationProblemDetails.Status = StatusCodes.Status422UnprocessableEntity;
-            //    validationProblemDetails.Title = "One or more validation errors occurred.";
-
-            //    return new UnprocessableEntityObjectResult(validationProblemDetails)
-            //    {
-            //        ContentTypes = { "application/problem+json" }
-            //    };
-            //} 
-            //else
-                throw new ArgumentException($"{response.Message};;{response.ValidationErrorsString}");
+            {
+                var validationProblemDetails = Utils.GetValidationProblemDetails(response.ValidationErrors.ToDictionary(err => err.Key, err => err.Value.ToArray()), HttpContext.Request.Path);
+                return new UnprocessableEntityObjectResult(validationProblemDetails)
+                {
+                    ContentTypes = { "application/problem+json" }
+                };
+            }
         }
 
         return CreatedAtRoute("GetCategoryById", new { id = response.Category.Id }, response.Category);
     }
+
+
 
     [HttpDelete("{id}")]
     public async Task<ActionResult> DeleteCategory(Guid id)
---
+++ Wenlin.API/Helpers/Utils.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Wenlin.API.Helpers;
+
+internal static class Utils
+{
+    internal static ValidationProblemDetails GetValidationProblemDetails(IDictionary<string, string[]> errors, string instance)
+    {
+        var validationProblemDetails = new ValidationProblemDetails(errors);
+
+        // add additional info not added by default
+        validationProblemDetails.Detail = "See the errors field for details.";
+        validationProblemDetails.Instance = instance;
+
+        // report invalid model state responses as validation issues
+        validationProblemDetails.Type = "https://courselibrary.com/modelvalidationproblem";
+        validationProblemDetails.Status = StatusCodes.Status422UnprocessableEntity;
+        validationProblemDetails.Title = "One or more validation errors occurred.";
+
+        return validationProblemDetails;
+    }
+}

678af083d5823328c5db3d4041d5e901eeeecbc7
Wenlin Huang <jackhwl@hotmail.com>
2023-04-13 19:09:38 -0400

    add datetimeoffsetExtension to get Age

---
+++ Wenlin.Application/Helpers/DateTimeOffsetExtensions.cs
@@ -0,0 +1,16 @@
+﻿namespace Wenlin.Application.Helpers;
+public static class DateTimeOffsetExtensions
+{
+    public static int GetCurrentAge(this DateTimeOffset dateTimeOffset)
+    {
+        var currentDate = DateTime.UtcNow;
+        int age = currentDate.Year - dateTimeOffset.Year;
+
+        if (currentDate < dateTimeOffset.AddYears(age))
+        {
+            age--;
+        }
+
+        return age;
+    }
+}

625cbd7e6f1012f49c5c55134a61cd28cfca41c2
Wenlin Huang <jackhwl@hotmail.com>
2023-04-17 13:59:35 -0400

    Add Image model

---
+++ Wenlin.Domain/Configuration/ImageConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Wenlin.Domain.Entities;
+
+namespace Wenlin.Domain.Configuration;
+public class ImageConfiguration : IEntityTypeConfiguration<Image>
+{
+    public void Configure(EntityTypeBuilder<Image> builder)
+    {
+        builder
+            .Property(p => p.Title)
+            .IsRequired()
+            .HasMaxLength(150);
+        builder
+            .Property(p => p.FileName)
+            .IsRequired()
+            .HasMaxLength(200);
+        builder
+            .Property(p => p.OwnerId)
+            .IsRequired()
+            .HasMaxLength(50);
+    }
+}
---
+++ Wenlin.Domain/Entities/Image.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Wenlin.Domain.Entities;
+public class Image
+{
+    [Key]
+    public Guid Id { get; set; }
+
+    public string Title { get; set; } = string.Empty;
+
+    public string FileName { get; set; } = string.Empty;
+
+    public string OwnerId { get; set; } = string.Empty;
+}

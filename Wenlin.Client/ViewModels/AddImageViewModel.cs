c0fb2d1aa806cd4edd36e608c8ef86d6bc98549f
Wenlin Huang <jackhwl@hotmail.com>
2023-04-19 07:07:06 -0400

    supress CS8618

--- Wenlin.Client/ViewModels/AddImageViewModel.cs
+++ Wenlin.Client/ViewModels/AddImageViewModel.cs
@@ -15,7 +15,9 @@ public class AddImageViewModel
         Files = files;
     }
 
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
     public AddImageViewModel()
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
     {
 
     }

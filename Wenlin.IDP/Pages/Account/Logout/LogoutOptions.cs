1f0e3df3e5ad0d4410d62583b607fbf0c2a8c7a3
Wenlin Huang <jackhwl@hotmail.com>
2023-04-19 07:54:14 -0400

    Add IDP UI Pages Template

---
+++ Wenlin.IDP/Pages/Account/Logout/LogoutOptions.cs
@@ -0,0 +1,8 @@
+
+namespace Wenlin.IDP.Pages.Logout;
+
+public class LogoutOptions
+{
+    public static bool ShowLogoutPrompt = true;
+    public static bool AutomaticRedirectAfterSignOut = false;
+}

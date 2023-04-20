3810c2baa54b6d8a08b3960e8ac9302132154563
Wenlin Huang <jackhwl@hotmail.com>
2023-04-20 09:07:57 -0400

    Logging out of Our Web Application, (not IDP)

---
+++ Wenlin.Client/Controllers/AuthenticationController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Wenlin.Client.Controllers;
+public class AuthenticationController : Controller
+{
+    [Authorize]
+    public async Task Logout()
+    {
+        // clears the local cookie
+        await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+    }
+}

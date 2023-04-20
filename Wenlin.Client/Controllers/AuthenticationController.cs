ad56a10b3aca92af5120d8dbf8038da362c9b855
Wenlin Huang <jackhwl@hotmail.com>
2023-04-20 09:16:40 -0400

    Logging out of the Identity Provider

--- Wenlin.Client/Controllers/AuthenticationController.cs
+++ Wenlin.Client/Controllers/AuthenticationController.cs
@@ -12,5 +12,9 @@ public class AuthenticationController : Controller
     {
         // clears the local cookie
         await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+
+        // Redirects to the IDP linked to scheme "OpenIdConnectDefaults.AuthenticationScheme" (oidc)
+        // so it can clear its own session/cookie
+        await HttpContext.SignOutAsync(OpenIdConnectDefaults.AuthenticationScheme);
     }
 }

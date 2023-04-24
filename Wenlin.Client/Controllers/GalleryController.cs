63afdbae4a0877bee26e3626a7efa993dfbcad5c
Wenlin Huang <jackhwl@hotmail.com>
2023-04-24 16:01:31 -0400

    Role-based Authorization: Using the Role in Your Controllers

--- Wenlin.Client/Controllers/GalleryController.cs
+++ Wenlin.Client/Controllers/GalleryController.cs
@@ -47,9 +47,60 @@ public class GalleryController : Controller
         }
     }
 
+    [Authorize(Roles = "PayingUser")]
     public IActionResult AddImage()
     {
         return View();
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    [Authorize(Roles = "PayingUser")]
+    public async Task<IActionResult> AddImage(AddImageViewModel addImageViewModel)
+    {
+        if (!ModelState.IsValid)
+        {
+            return View();
+        }
+
+        //// create an ImageForCreation instance
+        //ImageForCreation? imageForCreation = null;
+
+        //// take the first (only) file in the Files list
+        //var imageFile = addImageViewModel.Files.First();
+
+        //if (imageFile.Length > 0)
+        //{
+        //    using (var fileStream = imageFile.OpenReadStream())
+        //    using (var ms = new MemoryStream())
+        //    {
+        //        fileStream.CopyTo(ms);
+        //        imageForCreation = new ImageForCreation(
+        //            addImageViewModel.Title, ms.ToArray());
+        //    }
+        //}
+
+        //// serialize it
+        //var serializedImageForCreation = JsonSerializer.Serialize(imageForCreation);
+
+        //var httpClient = _httpClientFactory.CreateClient("APIClient");
+
+        //var request = new HttpRequestMessage(
+        //    HttpMethod.Post,
+        //    $"/api/images")
+        //{
+        //    Content = new StringContent(
+        //        serializedImageForCreation,
+        //        System.Text.Encoding.Unicode,
+        //        "application/json")
+        //};
+
+        //var response = await httpClient.SendAsync(
+        //    request, HttpCompletionOption.ResponseHeadersRead).ConfigureAwait(false);
+
+        //response.EnsureSuccessStatusCode();
+
+        return RedirectToAction("Index");
     }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
